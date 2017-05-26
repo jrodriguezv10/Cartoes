5ce741b51f394fdf4951e3042de74d263f22d84e
jrodriguezv10 <jrodriguezv10@gmail.com>
2017-05-26 09:30:51 -0300

    change star location

--- js/cartoes.js
+++ js/cartoes.js
@@ -18,8 +18,8 @@ google.maps.event.addDomListener(window, 'load', initializedMap);
 function initializedMap() {
   var mapCanvas = document.getElementById('map');
   var mapOptions = {
-    center: new google.maps.LatLng(-25.383948, -49.246980),
-    zoom: 15,
+    center: new google.maps.LatLng(-25.467328, -49.254101),
+    zoom: 13,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     scrollwheel: false,
     styles: mapStylesGray
